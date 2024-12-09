fcb4aadd4bb66acfe89bbd7bfa7cc5c826e8581a
AndK37 <146865715+AndK37@users.noreply.github.com>
2024-12-09 18:58:03 +0500

    final build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/weather_vue/dist/' : '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/weather-vue-pages/" : "/",
   pluginOptions: {
     i18n: {
       locale: 'en',
